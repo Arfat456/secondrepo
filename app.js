b1af6bc49b6920b667a4fbc0f56e05aca4738f2a
unknown <aa3919168@gmail.com>
2024-01-06 17:24:35 +0500

    DONE

---
+++ app.js
@@ -0,0 +1,706 @@
+//chapter 1 
+
+//Q no 1
+//I. First Name
+//var firstName = "Arfat";
+// alert("First Name: " + firstName);
+
+//II. Last Name
+// var lastName = "Jameel";
+// alert("Last Name: " + lastName);
+
+//III. Email
+// var email = "aa3919168@gmail.com";
+// alert("Email: " + email);
+
+//IV. Phone Number
+// var phoneNumber = "+92 3212801182";
+// alert("Phone Number: " + phoneNumber);
+
+//V. Password
+// var password = "jsassignmnet";
+// alert("Password: " + password);
+
+//Q no 2
+//alert("You are learning JavaScript");
+
+//Q no 3
+//alert("javascript Mini Test");
+
+
+//chapter no 2 
+
+// Q no 1
+// var myVariableName = "cameless Varaible";
+
+// Q no 2
+// var myVariable;
+// myVariable = "string Assigned";
+
+// Q no 3 
+// var teamName = "Pakistan Team";
+// alert("Our team name is: " + teamName);
+
+// Q no 4 
+// var bestMan = "Charlie";
+// bestMan = "FK";
+
+// chapter no 3 
+
+// Q no 1 
+// var caseQty;
+// caseQty = 10; 
+
+// Q no 2
+// var caseQty; 
+// caseQty = 144;
+
+// Q no 3
+// var num = 9; 
+// var num = parseInt("9");
+// var num = parseInt("9");
+
+// Q no 4
+// var sumResult;
+// sumResult = 5 + 7;
+// Now the final result (12 in this case)
+
+//Q no 5
+// var merchTotal = 100;
+// var shippingCharge = 10;
+// var orderTotal = merchTotal + shippingCharge;
+
+// console.log("The value of orderTotal is: " + orderTotal);
+// merchTotal + shippingCharge = 100 + 10 = 110
+
+// Q no 6
+// let myVariable = 10;
+// myVariable = myVariable + 5;
+// console.log(myVariable);
+
+
+// chapter no 4
+
+// Q no 1
+// var productCost = 3.45;
+
+// Q no 2
+// var nameOfBand;
+
+// Q no 3
+// let myNumber = 42;
+
+// Q no 4
+// let myFullName = "ArfatJameel";
+
+//Q no 5
+// Legal variable names
+// let myVariable = 42;
+// let user123 = "Arfat";
+// let total_amount = 100.5;
+// let _count = 5;
+// let $name = "Jameel";
+// let camelCaseExample = "Hello";
+
+// Illegal variable names
+// let 123variable = "Invalid"; // starts with a number
+// let my variable = "Invalid"; // contains space
+// let user@name = "Invalid"; // contains special character @
+// let if = "Invalid"; // reserved keyword
+
+
+// chapter no 5
+
+// Q no 1
+// let remainder = 10 % 3; 
+
+// Q no 2
+// var num = 20 % 6; 
+//is 2
+
+// Q no 3
+// let largeNum = 1000 * 2000;
+// largeNum = 2,000,000
+
+// Q no 4
+// let firstVariable = 10;
+// let secondVariable = 5;
+// let result = firstVariable - secondVariable;
+// console.log(result); is 5
+
+// Q no 5
+// let remainderValue = 15 % 4;
+// console.log(remainderValue); is 3
+
+
+//Q no 6
+// let number1 = 5;
+// let number2 = 8;
+// let result = number1 * number2;
+// alert("The result of multiplication is: " + result);
+
+// chapter no 6
+
+// Q no 1
+// x++;
+
+// Q no 2 
+// x--;
+
+// Q no 3
+// var x = 50;
+// var y = x++;(the value of y is 50)
+
+// Q no 4
+// var y = 50;
+// var z = --y;(th value of z is 49)
+
+// Q no 5
+// var newNum = --num;
+
+// Q no 6
+// var originalValue = variable++;
+
+// Q no 7
+// var myNumber = 5;
+// myNumber++;
+// alert("The new value is: " + myNumber);
+
+// chapter no 7
+
+// Q no 1
+// var calculatedNum = 2 + (2 * 6);
+// (the answer is 14)
+
+// Q no 2
+// var calculatedNum = (2 + 2) * 6;
+// (the answer is 24)
+
+// Q no 3
+// var calculatedNum = (2 + 2) * (4 + 2);
+// (the answer is 24)
+
+// Q no 4 
+// var calculatedNum = ((2 + 2) * 4) + 2;
+// (the answer is 18)
+
+// Q no 5
+// var cost = (2 + 2) * 4 + 10;
+// (the cost value  is 26)
+
+// Q no 6
+// var units = 2 + (2 * 4) + 10;
+// (the units of value is 20)
+
+// Q no 7
+// var pressure = (4 / 2) * 4;
+// (the value of pressure is 8)
+
+
+// chapter no 8 
+
+// Q no 1
+// var num = "2" + "2";
+// (the answer is "22")
+
+// Q no 2
+// message = ("Hello," + "Dolly");(the answer is "hello,Dolly")
+// alert(message);
+
+// Q no 3
+// alert("33" + 3);
+// (its display the message "333" because of quotation marks);
+
+// Q no 4
+// var part1 = "Pakistan ";
+// var part2 = "Zindabad";
+
+// alert(part1 + part2);
+
+
+// Q no 5
+// var stringPart = "Hello, ";
+// var numberPart = 42;
+
+// var concatenatedString = stringPart + numberPart;
+
+// Q no 6
+// var firstString = "Hello, ";
+// var secondString = "world!";
+// var concatenatedString = firstString + secondString;
+
+// console.log(concatenatedString);
+
+
+
+// chapter no 9
+
+// Q no 1
+// var firstName = prompt("Enter first name");
+
+//Q no 2
+//var country = prompt("Country?", "China");
+
+// Q no 3
+// var yourName = prompt("Enter Your Name");
+
+//Q no 4
+//var userInput = prompt("Please enter your response:", "Default Input");
+
+// Q no 5
+// var firstPromptMessage = "Enter your first name:";
+// var secondPromptDefault = "Arfat";
+
+// var userResponse = prompt(firstPromptMessage, secondPromptDefault);
+
+// //Q no 6
+// var promptMessage = "Enter your favorite color:";
+// var defaultResponse = "blue";
+
+
+// var userResponse = prompt(promptMessage, defaultResponse);
+// alert("Your favorite color is: " + userResponse);
+
+
+//chapter  no 10
+
+//Q no 1
+// var city = "Karachi";
+// if (city === "Karachi") {
+//   console.log("The City of Lights");
+// }
+
+// Q no 2
+// if (x === y) {
+//     var z = prompt("Enter the value of z?");
+//   }
+
+// Q no 3
+// var ZipCode = "10010";
+
+// if (ZipCode === "10010") {
+//   alert("Karachi");
+// } else {
+//   alert("Please write correct city");
+// }
+
+// Q no 4
+// var x = 1;  
+// if (x === 1) {
+//    x = 42;  
+// }
+
+// console.log("The value of x is: " + x);
+
+
+// chapter no 11
+
+// Q no 1
+// if (variable1 !== variable2) {
+//     //code here if the condition is true
+//   }
+
+// Q no 2
+// if (variable1 >= variable2) {
+//     // code here if the condition is true
+//   }
+  
+//Q no 3
+//let myNumber = 10;
+//const particularNumber = 42;
+
+//if (myNumber !== particularNumber) {
+  // Assign a new number to 'myNumber'
+  //myNumber = /* new value */;
+//}
+
+//Q NO 4
+//let yourNumber = 20;
+//const differentNumber = 10;
+
+//if (yourNumber !== differentNumber) {
+  // Display a congratulations alert
+  //alert("Congratulations!");
+//}
+
+// Q no 5
+// const enteredName = prompt("Please enter your first name:");
+// const anotherName = "arfat";
+// if (enteredName !== anotherName) {
+//   // Display a "No match" alert
+//   alert("No match");
+// }
+
+
+//chapter no 12 
+ //Q  no 1 
+//let variable1 = 25;
+//let variable2 = 30;
+//if (variable1 >= variable2) {
+  // Display an alert if variable1 is greater than or equal to variable
+  //alert("Variable1 is greater than or equal to Variable2");
+//} else 
+  // Display a different alert if variable1 is not greater than or equal to variable2
+  //alert("Variable1 is less than Variable2");
+//}
+
+// Q no 2
+// let subject1 = parseFloat(prompt("Enter marks for subject 1:"));
+// let subject2 = parseFloat(prompt("Enter marks for subject 2:"));
+// let subject3 = parseFloat(prompt("Enter marks for subject 3:"));
+
+// let totalMarks = subject1 + subject2 + subject3;
+// let percentage = (totalMarks / 300) * 100;
+
+
+// let grade;
+
+// if (percentage >= 90) {
+//   grade = 'A';
+// } else if (percentage >= 80) {
+//   grade = 'B';
+// } else if (percentage >= 70) {
+//   grade = 'C';
+// } else if (percentage >= 60) {
+//   grade = 'D';
+// } else {
+//   grade = 'F';
+// }
+
+// alert(`Total Marks: ${totalMarks}\nPercentage: ${percentage.toFixed(2)}%\nGrade: ${grade}`);
+
+// Q no 3
+// if (a === 10) {
+//     alert("a is 10");
+//   } else {
+//     alert("The correct value is not 10");
+//   }
+
+//  Q no 4 
+//  let userCity = prompt("Enter a city:");
+
+// if (userCity.toLowerCase() === "karachi") {
+//   alert("Acknowledging it is Karachi.");
+// } else if (userCity.toLowerCase() === "lahore") {
+//   alert("Acknowledging it's Lahore.");
+// } else {
+//   alert("City not recognized.");
+// }
+
+
+
+//chapter no 13
+
+//Q no 1 
+//if (a === b && c === d){
+    //both condition are true 
+//}
+  
+//Q no 2
+//if (a === b || c !== d) {
+    // both conditions are true
+  //}
+
+//Q no 3
+//if ((name === "Hamza" || name === "Arsalan")&& age < 60){
+    //code here or both conditions are true 
+//}
+  
+//Q no 4  
+//let firstNumber = 5;
+//let secondNumber = 10;
+
+// if (firstNumber < secondNumber || firstNumber > secondNumber) {
+//   alert("The condition is true. The first variable is less than or greater than the second variable.");
+// } else {
+//   alert("The condition is false. The first variable is not less than or greater than the second variable.");
+// }
+
+// Q no 5
+// let firstName = "YourFirstName";
+// let lastName = "YourLastName";
+
+// let userFirstName = prompt("Enter your first name:");
+
+// let userLastName = prompt("Enter your last name:");
+
+// if (userFirstName === firstName && userLastName === lastName) {
+//   alert("Names match! You entered your correct first and last names.");
+// } else {
+//   alert("Names do not match. Please double-check your input.");
+//}
+
+
+// chapter no 14
+
+// Q no 1
+// let password = /* Get the password value from somewhere;
+
+//if (password !== "") {
+//   if (password.length <= 5) {
+//     alert("Password must be greater than 5.");
+//   } else {
+//     alert("OK");
+//   }
+// } else {
+//   alert("Password cannot be empty.");
+// }
+
+// Q no 2
+// if (a === 1) {
+//     if (c === "Max") {
+//       alert("OK");
+//     }
+//   }
+  
+// Q no 3
+// if (a === 1 && c === "Max") {
+//     alert("OK");
+//   }
+
+// Q no 4
+// let firstVariable = 42;
+// let secondVariable = 42;
+
+// if (firstVariable === secondVariable) {
+//   if (firstVariable <= secondVariable) {
+//     alert("Both conditions are true!");
+//   }
+// }
+
+
+
+// chapter no 15 
+
+// Q no 1
+// let emptyArray = [];
+
+// Q no 2
+// let stringArray = ["Hello, World!"];
+
+// Q no 3
+// var alphabet = ["h", "i", "j", "k"];
+
+// alert(alphabet[2]);
+
+// Q no 4
+// var alphabet = ["h", "i", "j", "k", "l", "m", "n", "o"];
+
+// var arrayLength = alphabet.length;
+
+// alert("The total length of the array is: " + arrayLength);
+
+// Q no 5
+// let myArray = ["firstElement"];
+
+// myArray[1] = "secondElement";
+// alert(myArray[1]);
+
+
+// chapter no 16
+
+// Q no 1 
+// let myArray = ["firstElement"];
+
+// myArray.push("secondElement");
+
+// alert(myArray[myArray.length - 1]);
+  
+// Q no 2
+// var Alphabet = ["h", "i", "j", "k"];
+
+// Alphabet.pop();
+
+// alert(Alphabet);
+
+// Q no 3
+// var Alphabet = ["h", "i", "j", "k"];
+
+// Alphabet.push(42);
+
+// alert(Alphabet);
+
+
+  
+// chapter no 16(2)
+
+// Q no 1
+// var sizes = ["S", "M", "XL", "XXL", "XXXL"];
+
+// sizes.shift();
+
+// alert(sizes);
+
+// Q no 2
+// var sizes = ["S", "M", "XL", "XXL", "XXXL"];
+
+// sizes.unshift(1, 2, 3);
+// alert(sizes);
+
+// Q no 3
+// let myArray = ["originalElement"];
+
+// myArray.unshift("newElement");
+
+// alert(myArray[0]);
+
+// Q no 4
+// var sizes = ["S", "M", "XL", "XXL", "XXXL"];
+
+// var indexM = sizes.indexOf("M");
+
+// sizes.splice(indexM + 1, 0, "L");
+
+// alert(sizes);
+
+// Q no 5
+// var sizes = ["S", "M", "XL", "XXL", "XXXL"];
+
+// var regSizes = sizes.slice(0, 3);
+
+// alert(regSizes);
+
+// Q no 6
+// var pets = ["dog", "cat", "ox", "duck", "frog"];
+
+// var indexDog = pets.indexOf("dog");
+// pets.splice(indexDog + 1, 3, "elephant", "giraffe");
+
+// alert(pets);
+
+// Q no 17
+// var pets = ["dog", "cat", "ox", "duck", "frog"];
+
+// var indexCat = pets.indexOf("cat");
+// var indexOx = pets.indexOf("ox");
+
+// pets.splice(indexCat, 1);
+// pets.splice(indexOx, 1);
+
+// alert(pets);
+
+// Q no 18
+// var pets = ["dog", "cat", "ox", "duck", "frog", "flea"];
+
+// var indexDuck = pets.indexOf("duck");
+// var indexFrog = pets.indexOf("frog");
+
+// var reducedPets = pets.slice(indexDuck, indexFrog + 1);
+
+// alert(reducedPets);
+
+
+
+//chapter no 17-20
+
+// Q no 1
+// for (let i = 0; i < 10; i++) {
+    // your code here (this will run 10 times)
+    //console.log("Iteration: " + i);
+  //}
+
+//Q no 2
+//for (let i = 0; i <= 11; i++) {
+    // your code here (this will run 12 times)
+  //  console.log("Iteration: " + i);
+  //}
+
+ //Q no 3
+ //for (var i = 0; i <= 4; i++)
+ 
+//Q no 4
+//for (let counter = 0; counter < 100; counter++) {
+    // Your code here (this will run 100 times)
+  //  console.log("Iteration: " + counter);
+ // }
+  
+//Q no 5
+//for (let i = 3; i > 0; i--) {
+    // Your code here (this will run 3 times)
+  //  console.log("Iteration: " + i);
+ // }
+  
+//Q no 6
+// let myArray = [1, 2, 3, 4, 5];
+
+// let arrayLength = myArray.length;
+
+// alert("The number of elements in the array is: " + arrayLength);
+
+// Q no 7
+// let flag = true; 
+
+// Q no 8
+// for (let i = 0; i < pets.length; i++) {
+    // Your code here (this  run for each element in the 'pets' array)
+   // console.log("Iteration: " + i);
+ // }
+  
+// Q no 9 
+// for (let i = 0; i < 10; i++) {
+//     if (i === 1) {
+      
+//       alert("Counter value on the second iteration: " + i);
+      
+//       break;
+//     }
+//   }
+
+//Q no 10
+//let userNames = ["Arfat", "Muzammil", "Ahmed", "Fahad"];
+
+// let firstName = prompt("Enter first name:");
+
+// for (let i = 0; i < userNames.length; i++) {
+  
+//   if (firstName === userNames[i]) {
+//      alert("Welcome, " + firstName + "!");
+//     break; 
+//   } else {
+//      alert("Please write the correct user name.");
+//     break; 
+//   }
+// }
+
+// Q no 11
+// var matchFound = false;
+
+// for (var i = 0; i < list.length; i++) {
+//   if (userInput === list[i]) {
+//     alert("Match found");
+//     matchFound = true;
+//     break;
+//   }
+// }
+
+// if (!matchFound) {
+//   alert("Match not found. Please enter a correct value.");
+// }
+
+// Q no 12
+// var firstArr = ["a", "b", "c", "d", "e", "f"];
+// var secondArr = [1, 2, 3, 4, 5, 6];
+
+// for (var i = 0; i < firstArr.length; i++) {
+
+// for (var j = 0; j < secondArr.length; j++) {
+    
+//     console.log(firstArr[i] + secondArr[j]);
+//   }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
